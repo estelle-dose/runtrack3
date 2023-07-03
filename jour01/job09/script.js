fc4e290e66e5dd85633bb261b0876783d89d1b19
Estelle Dose <estelle.dose@laplateforme.io>
2023-07-03 11:11:18 +0200

    tri

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -1,16 +1,44 @@
-function fizzbuzz() {
-    for (var i = 1; i <= 151; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0) {
-            console.log("Fizz");
-        } else if (i % 5 === 0) {
-            console.log("Buzz");
-        } else {
-            console.log(i);
-        }
+function tri(numbers, order) {
+    if (order === "asc") {
+        return triAscendant(numbers);
+    } else if (order === "desc") {
+        return triDescendant(numbers);
+    } else {
+        console.log("Paramètre 'order' invalide. Veuillez utiliser 'asc' ou 'desc'.");
+        return numbers;
     }
 }
 
-// Appel de la fonction fizzbuzz
-fizzbuzz();
+// Fonction de tri ascendant
+function triAscendant(numbers) {
+    for (var i = 0; i < numbers.length - 1; i++) {
+        for (var j = i + 1; j < numbers.length; j++) {
+            if (numbers[j] < numbers[i]) {
+                var temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+    return numbers;
+}
+
+// Fonction de tri descendant
+function triDescendant(numbers) {
+    for (var i = 0; i < numbers.length - 1; i++) {
+        for (var j = i + 1; j < numbers.length; j++) {
+            if (numbers[j] > numbers[i]) {
+                var temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+    return numbers;
+}
+
+// Exemple d'utilisation de la fonction
+var numbers = [7, 2, 9, 1, 5];
+console.log(tri(numbers, "asc")); // [1, 2, 5, 7, 9] (tri ascendant)
+console.log(tri(numbers, "desc")); // [9, 7, 5, 2, 1] (tri descendant)
+
