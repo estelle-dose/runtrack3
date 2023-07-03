e737ec082fba4eee7b48dc4b443f5192c9b0bcec
Estelle Dose <estelle.dose@laplateforme.io>
2023-07-03 08:54:36 +0200

    script.js

---
+++ jour01/job02/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma page</title>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+    <!-- Contenu de la page -->
+</body>
+</html>
+
