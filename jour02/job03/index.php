4d504f24b1f52ff4bc2ef7aa5f82c25e4ff70ee8
Estelle Dose <estelle.dose@laplateforme.io>
2023-07-04 09:44:39 +0200

    compteur

---
+++ jour02/job03/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Compteur</title>
+</head>
+<body>
+  <button id="button">Cliquez ici</button>
+  <p id="compteur">0</p>
+  
+  <script src="script.js"></script>
+</body>
+</html>
+
