84b92d6163e129c919dd17133efc1ce3a6cacae3
Estelle Dose <estelle.dose@laplateforme.io>
2023-07-04 08:37:37 +0200

    citation

---
+++ jour02/job01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Exemple Citation</title>
+  <script src="script.js"></script>
+</head>
+<body>
+  <article id="citation">"La vie a beaucoup plus d’imagination que nous"</article><br>
+  <button id="button" onclick="citation()">Afficher la citation dans la console de développement.</button>
+</body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,6 @@
+function citation() {
+    var citationElement = document.getElementById("citation");
+    var citationText = citationElement.textContent;
+    console.log(citationText);
+  }
+  
